0747ead1a45a103c7cfc0ce1556dc6b0bb8473c9
TabogaBr <brenda.taboga7@gmail.com>
2022-10-06 20:31:19 +0300

    Exercise complete

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { useState } from 'react';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+
+  const [text, setText] = useState('');
+
+  const speak = () => {
+    Speech.speak(text, { language: 'en' });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.input}
+        placeholder="To Hear"
+        onChangeText={text => setText(text)}
+        value={text} />
+      <Button title="PRESS TO HEAR TEXT" onPress={speak} />
     </View>
   );
 }
@@ -16,5 +27,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    width: 200,
+    height: 40,
   },
 });
